91cbc8e581c0ddfa41eda1ab79de6aa4b717d56b
Daniel Kumankumah <danielkumankumah@gmail.com>
2021-06-29 04:40:17 +0200

    layout fibonacci

--- app/src/main/java/nl/kumankumah/fibonacci_vr/SplashActivity.kt
+++ app/src/main/java/nl/kumankumah/fibonacci_vr/SplashActivity.kt
@@ -1,11 +1,25 @@
 package nl.kumankumah.fibonacci_vr
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
+
+        // Use Handler to wait 5 second before opening the MainActivity.
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(
+                Intent(
+                    this@SplashActivity,
+                    MainActivity::class.java
+                )
+            )
+            finish()
+        }, 5000)
     }
 }
