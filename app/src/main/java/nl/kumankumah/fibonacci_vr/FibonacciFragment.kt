e100d3d608f7c1766af6c5e74bc984b99ea029f3
Daniel Kumankumah <danielkumankumah@gmail.com>
2021-06-29 07:57:05 +0200

    fibonacci calculation added

--- app/src/main/java/nl/kumankumah/fibonacci_vr/FibonacciFragment.kt
+++ app/src/main/java/nl/kumankumah/fibonacci_vr/FibonacciFragment.kt
@@ -5,24 +5,58 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import nl.kumankumah.fibonacci_vr.databinding.FragmentFibonacciBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FibonacciFragment : Fragment() {
 
+    private var _binding: FragmentFibonacciBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var fibonacciAdapter: FibonacciAdapter
+    private var sequence = arrayListOf<Int>()
+
+    var f0 = 0
+    var f1 = 0
+    var result = 0
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fibonacci, container, false)
+        _binding = FragmentFibonacciBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        fibonacciAdapter = FibonacciAdapter(sequence)
+        initViews()
 
+        binding.btnNext.setOnClickListener{
+            result = when (sequence.size){
+                0 -> 0
+                1 -> 1
+                else -> fibonacciSequence()
+            }
+            binding.tvResult.text = getString(R.string.result, result)
+            sequence.add(result)
+            fibonacciAdapter.notifyDataSetChanged()
+        }
+    }
+
+    private fun initViews() {
+        binding.rvFibonacci.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        binding.rvFibonacci.adapter = fibonacciAdapter
+    }
+
+    fun fibonacciSequence(): Int {
+        f0 = f1
+        f1 = result
+        return f0 + f1
     }
 }
